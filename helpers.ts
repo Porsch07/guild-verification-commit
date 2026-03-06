3cee963f899a82198d637008166a4e3b3bc4b758
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-06 11:06:05 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:05:56.423Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T08:06:05.272Z
+Commit: Fix race condition in async operations
