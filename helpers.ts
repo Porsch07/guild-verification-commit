d473ce77a9529e3be48b9991edb787ff61ea22a3
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-06 11:05:07 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:03:53.849Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T08:05:06.761Z
+Commit: Fix memory leak in event handlers
