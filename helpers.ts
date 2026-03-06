330f9ed3dd1b6dec86150e9ea0fd127e3857db71
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-06 11:03:54 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:03:49.993Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T08:03:53.849Z
+Commit: Fix broken links in documentation
