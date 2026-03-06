c811ae29474e8d22f2b7e7145ff799a749d9127a
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-06 11:05:51 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:05:40.102Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T08:05:50.606Z
+Commit: Fix broken links in documentation
