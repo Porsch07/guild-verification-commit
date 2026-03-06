56d18aa2d88cf7d2a5c12aeba23c2ea1ed89b5d4
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-06 11:05:40 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:05:33.036Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T08:05:40.102Z
+Commit: Resolve null pointer exception in user service
