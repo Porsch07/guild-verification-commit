371b5c04ea5c47cfbae3fabf4c15c6acb3370acd
doctor <132792258+Porsch07@users.noreply.github.com>
2026-03-26 10:16:46 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:06:01.868Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-26T07:16:45.622Z
+Commit: Fix broken links in documentation
